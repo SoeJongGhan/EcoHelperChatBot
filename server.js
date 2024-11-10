6bd4892bbe1a0cedfb2004763d657f04063bca12
Dair <147152353+SoeJongGhan@users.noreply.github.com>
2024-11-10 22:10:00 +0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,85 @@
+// node --version # Should be >= 18
+// npm install @google/generative-ai express
+
+const express = require('express');
+const { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } = require('@google/generative-ai');
+const dotenv = require('dotenv').config()
+
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(express.json());
+const MODEL_NAME = "gemini-pro";
+const API_KEY = process.env.API_KEY;
+
+async function runChat(userInput) {
+  const genAI = new GoogleGenerativeAI(API_KEY);
+  const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+
+  const generationConfig = {
+    temperature: 0.9,
+    topK: 1,
+    topP: 1,
+    maxOutputTokens: 1000,
+  };
+
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    // ... other safety settings
+  ];
+
+  const chat = model.startChat({
+    generationConfig,
+    safetySettings,
+    history: [
+      {
+        role: "user",
+        parts: [{ text: "Ты Eco Helper Chat Bot🌎 Твоя задача помогать людям из города Актобе в сортировке мусора и давать советы как это делать" }],
+      },
+      {
+        role: "model",
+        parts: [{ text: "Привет! Я Eco Helper Chat Bot 🌎, рад помочь жителям Актобе с сортировкой мусора! Давайте вместе сделаем наш город чище и зеленее!" }],
+      },
+      {
+        role: "user",
+        parts: [{ text: "Привет" }],
+      },
+      {
+        role: "model",
+        parts: [{ text: "Привет! Чем я могу тебе помочь сегодня? Хочешь узнать, как сортировать мусор в Актобе, или у тебя есть конкретный вопрос?" }],
+      },
+    ],
+  });
+
+  const result = await chat.sendMessage(userInput);
+  const response = result.response;
+  return response.text();
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+app.get('/loader.gif', (req, res) => {
+  res.sendFile(__dirname + '/loader.gif');
+});
+app.post('/chat', async (req, res) => {
+  try {
+    const userInput = req.body?.userInput;
+    console.log('incoming /chat req', userInput)
+    if (!userInput) {
+      return res.status(400).json({ error: 'Invalid request body' });
+    }
+
+    const response = await runChat(userInput);
+    res.json({ response });
+  } catch (error) {
+    console.error('Error in chat endpoint:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
